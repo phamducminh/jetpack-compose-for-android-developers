bf19247e8b9b99ac1b0c1047df715e18c0b47b61
Minh Pham <pdminh0410@gmail.com>
2024-08-24 17:03:29 -0700

    State in Jetpack Compose: Observable MutableList

--- BasicStateCodelab/app/src/main/java/com/example/basicstatecodelab/WellnessScreen.kt
+++ BasicStateCodelab/app/src/main/java/com/example/basicstatecodelab/WellnessScreen.kt
@@ -1,11 +1,30 @@
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Modifier
+import com.example.basicstatecodelab.WellnessTask
 
 @Composable
 fun WellnessScreen(modifier: Modifier = Modifier) {
     Column(modifier = modifier) {
         StatefulCounter()
-        WellnessTaskList()
+
+        // Don't do this!
+//        val list = remember { mutableStateListOf<WellnessTask>() }
+//        list.addAll(getWellnessTasks())
+
+        // Do this instead. Don't need to copy
+//        val list = remember {
+//            mutableStateListOf<WellnessTask>().apply { addAll(getWellnessTasks()) }
+//        }
+
+        val list = remember { getWellnessTasks().toMutableStateList() }
+        WellnessTaskList(
+            list = list,
+            onCloseTask = { task -> list.remove(task)}
+        )
     }
 }
+
+fun getWellnessTasks() = List(30) { i -> WellnessTask(i, "Task # $i") }
