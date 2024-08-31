d1d91124b007cdae474345e3b98d4630f8becf59
Minh Pham <pdminh0410@gmail.com>
2024-08-31 14:21:01 -0700

    Theming in Compose with Material 3: Emphasis

--- ThemingCodelab/app/src/main/java/com/example/reply/ui/components/ReplyEmailListItem.kt
+++ ThemingCodelab/app/src/main/java/com/example/reply/ui/components/ReplyEmailListItem.kt
@@ -82,7 +82,8 @@ fun ReplyEmailListItem(
                     )
                     Text(
                         text = email.createdAt,
-                        style = MaterialTheme.typography.labelMedium
+                        style = MaterialTheme.typography.labelMedium,
+                        color = MaterialTheme.colorScheme.onSurfaceVariant
                     )
                 }
                 IconButton(
@@ -107,6 +108,7 @@ fun ReplyEmailListItem(
                 text = email.body,
                 maxLines = 2,
                 style = MaterialTheme.typography.bodyLarge,
+                color = MaterialTheme.colorScheme.onSurfaceVariant,
                 overflow = TextOverflow.Ellipsis
             )
         }
