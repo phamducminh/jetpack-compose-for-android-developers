3854b12958699db6380f10f5d2226be10b45611b
Minh Pham <pdminh0410@gmail.com>
2024-09-09 19:35:48 -0700

    Build adaptive apps with Jetpack Compose: Dynamic navigation

--- AdaptiveUiCodelab/app/src/main/java/com/example/reply/ui/ReplyApp.kt
+++ AdaptiveUiCodelab/app/src/main/java/com/example/reply/ui/ReplyApp.kt
@@ -16,23 +16,22 @@
 
 package com.example.reply.ui
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Text
+import androidx.compose.material3.adaptive.currentWindowAdaptiveInfo
+import androidx.compose.material3.adaptive.currentWindowSize
+import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffold
+import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffoldDefaults
+import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteType
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.toSize
 import com.example.reply.data.Email
 
 @Composable
@@ -55,20 +54,22 @@ private fun ReplyNavigationWrapperUI(
     var selectedDestination: ReplyDestination by remember {
         mutableStateOf(ReplyDestination.Inbox)
     }
+    val windowSize = with(LocalDensity.current) {
+        currentWindowSize().toSize().toDpSize()
+    }
+    val layoutType = if (windowSize.width >= 1200.dp) {
+        NavigationSuiteType.NavigationDrawer
+    } else {
+        NavigationSuiteScaffoldDefaults.calculateFromAdaptiveInfo(
+            currentWindowAdaptiveInfo()
+        )
+    }
 
-    // You will implement adaptive navigation here.
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(MaterialTheme.colorScheme.inverseOnSurface)
-    ) {
-        Box(modifier = Modifier.weight(1f)) {
-            content()
-        }
-
-        NavigationBar(modifier = Modifier.fillMaxWidth()) {
+    NavigationSuiteScaffold(
+        layoutType = layoutType,
+        navigationSuiteItems = {
             ReplyDestination.entries.forEach {
-                NavigationBarItem(
+                item(
                     selected = it == selectedDestination,
                     onClick = { /*TODO update selection*/ },
                     icon = {
@@ -83,6 +84,8 @@ private fun ReplyNavigationWrapperUI(
                 )
             }
         }
+    ) {
+        content()
     }
 }
 
